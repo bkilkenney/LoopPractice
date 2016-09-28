41665f3b6e49f843541591156f387d77e15d8e48
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-27 22:24:05 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //***********************************Most basic is while loop, "while something is true do this************************************
+            //Need to set a variable
+            //Need to increment it
+
+            int indiansWins = 75;
+            while (indiansWins < 90)
+            {
+                Console.WriteLine("We need 90 wins to get into the playoffs.  We have " +indiansWins+"wins!!!");
+                indiansWins++;
+            }
+
+            //***********************************************************************************************************************************
+
+
+        }
+    }
+}
+
+
